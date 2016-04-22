3f58980529f2045bee0ba7aa3af45db82b405a0f
Raja Sekar <rajasekar89be@gmail.com>
2016-04-22 10:50:56 +0530

    added opbeat

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({'extended':'true'}));            // parse applica
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
+app.use(opbeat.middleware.express())
 
 require('./app/router/router')(app);
 
