8615b91ddd6d7d4e60ade1359f16b5e162911b31
zhanghl-k <zhanghl-k@glodon.com>
2018-03-16 14:44:49 +0800

    mocha

--- app/config/config.js
+++ app/config/config.js
@@ -2,6 +2,7 @@ var config = {
 	port: process.env.PORT || 2000,
 	db: process.env.MONGOLAB_URI || "mongodb://localhost/todoapi",
 	test_port: 2001,
+	useMongoClient: false,
 	test_db: "mongodb://localhost/todoapi_test"
 }
 module.exports = config;
--- server.js
+++ server.js
@@ -8,14 +8,14 @@ var cors = require('cors');
 var app = express();
 
 var opbeat = require('opbeat').start({
-  appId: '873897962c',
-  organizationId: 'c1db293267274544b523bc8e0182443c',
-  secretToken: '3f4c16ad6b3f2453e9f5a04d94f58ce2fc081319'
+	appId: '873897962c',
+	organizationId: 'c1db293267274544b523bc8e0182443c',
+	secretToken: '3f4c16ad6b3f2453e9f5a04d94f58ce2fc081319'
 })
 
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(cors());
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
@@ -25,21 +25,22 @@ require('./app/router/router')(app);
 
 var db;
 
-if(process.env.NODE_ENV === "test"){
+if (process.env.NODE_ENV === "test") {
 	db = mongoose.connect(config.test_db);
-	app.listen(config.test_port, function(err){
-	  if(err) throw err;
-	  console.log("App listening on port "+config.test_port);
+	app.listen(config.test_port, function (err) {
+		if (err) throw err;
+		console.log("App listening on port " + config.test_port);
 	});
-}else{
- 	db = mongoose.connect(config.db);
-        app.listen(config.port, function(err){
-	  if(err) throw err;
-	  console.log("App listening on port "+config.port);
+} else {
+	db = mongoose.connect(config.db);
+	app.listen(config.port, function (err) {
+		if (err) throw err;
+		console.log("App listening on port " + config.port);
 	});
-
+}
 mongoose.connection.on('connected', function () {
-  console.log('Mongoose default connection open to ' + config.db);
+	console.log('Mongoose default connection open to ' + config.db);
 });
 
 module.exports = app;
+
